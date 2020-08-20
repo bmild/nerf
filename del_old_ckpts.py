62f89bb7aefe9286e5841ee2055884dd98b01180
Tianhao WU <tianhawu@kamzi-linux.cs.ucl.ac.uk>
2020-08-20 09:22:56 +0100

    Add script for removing old ckpts

---
+++ del_old_ckpts.py
@@ -0,0 +1,53 @@
+import os
+
+def del_ckpts(basedir='./logs/', expname=''):    
+    if expname == '':
+        exps = [exp
+                for exp in os.listdir(basedir)
+                if os.path.isdir(os.path.join(basedir, exp))]
+        exps.remove('summaries')
+
+        # print(exps)
+        for exp in exps:
+            del_ckpts(basedir,exp)
+        return
+        
+
+    ckpts_opt = [os.path.join(basedir, expname, f) 
+            for f in sorted(os.listdir(os.path.join(basedir, expname))) 
+            if 'optimizer' in f and f.endswith('.npy')]
+    
+    if len(ckpts_opt) == 0:
+        # no ckpts to remove
+        return
+    
+    last_cpkt_str = ckpts_opt[-1][-10:-4]
+
+    keeps = [os.path.join(basedir, expname, f) 
+        for f in os.listdir(os.path.join(basedir, expname)) 
+        if f.endswith(f'{last_cpkt_str}.npy')]
+
+    removes = [os.path.join(basedir, expname, f) 
+        for f in os.listdir(os.path.join(basedir, expname)) 
+        if f.endswith('.npy') and not f.endswith(f'{last_cpkt_str}.npy')]
+
+    print(f'\nDeleting old ckpts for experiment {expname}')
+    print(f'{len(keeps)} ckpts with iteration {last_cpkt_str} will be kept')
+    print(f'Other {len(removes)} cpkts will be removed')
+
+    confirm = input('Confirm?\n> ')
+    if confirm != 'yes' and confirm != 'y':
+        print('Aborting')
+        return
+
+    for f in removes:
+        os.remove(f)
+
+
+if __name__ == '__main__':
+    del_ckpts(expname='')
+
+
+
+
+
