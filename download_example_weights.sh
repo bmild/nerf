20a91e764a28816ee2234fcadb73bd59a613a44c
bmild <bmild@cs.berkeley.edu>
2021-01-21 07:41:22 -0800

    fix download links

--- download_example_weights.sh
+++ download_example_weights.sh
@@ -1,6 +1,6 @@
 mkdir -p logs
 cd logs
-wget https://people.eecs.berkeley.edu/~bmild/nerf/fern_example_weights.zip
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/fern_example_weights.zip
 unzip fern_example_weights.zip
-wget https://people.eecs.berkeley.edu/~bmild/nerf/lego_example_weights.zip
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/lego_example_weights.zip
 unzip lego_example_weights.zip
