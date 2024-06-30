e804b928e3277f694098ec45a0a588c44b945ee7
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-30 18:41:44 +0900

    Fix imread call to use apply_gamma=False instead of ignoregamma=True

--- load_llff.py
+++ load_llff.py
@@ -107,7 +107,7 @@ def _load_data(basedir, factor=None, width=None, height=None, load_imgs=True):
     
     def imread(f):
         if f.endswith('png'):
-            return imageio.imread(f, ignoregamma=True)
+            return imageio.imread(f, apply_gamma=False)
         else:
             return imageio.imread(f)
         
